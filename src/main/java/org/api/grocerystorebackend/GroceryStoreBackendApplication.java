c128886063e6537dd12f252e351edb6aca251905
npb <abcxyz@gmail.com>
2025-05-08 20:25:54 +0700

    Complete FlashSale API: Create auto, auto Active, auto Ended, get all

--- src/main/java/org/api/grocerystorebackend/GroceryStoreBackendApplication.java
+++ src/main/java/org/api/grocerystorebackend/GroceryStoreBackendApplication.java
@@ -2,8 +2,10 @@ package org.api.grocerystorebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"org.api.grocerystorebackend"})
+@EnableScheduling
 public class GroceryStoreBackendApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/api/grocerystorebackend/service/IFlashSaleService.java
+++ src/main/java/org/api/grocerystorebackend/service/IFlashSaleService.java
@@ -1,10 +1,19 @@
 package org.api.grocerystorebackend.service;
 
+import org.api.grocerystorebackend.dto.response.FlashSaleDTO;
+import org.api.grocerystorebackend.entity.FlashSale;
 import org.api.grocerystorebackend.entity.FlashSaleItem;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IFlashSaleService {
     Optional<FlashSaleItem> findProductInFlashSale(Long productId);
-    
+
+    void activeFlashSales();
+    void expireFlashSales();
+
+    FlashSaleDTO createProductInFlashSale();
+
+    List<FlashSaleDTO> getFlashSaleList();
 }
