5ddafb5bc1e9256677b5bb2aeea14fa58d1e84c7
npb <abcxyz@gmail.com>
2025-05-12 16:01:33 +0700

    Complete API getAllVoucher.

--- src/main/java/org/api/grocerystorebackend/entity/Voucher.java
+++ src/main/java/org/api/grocerystorebackend/entity/Voucher.java
@@ -1,13 +1,17 @@
 package org.api.grocerystorebackend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.api.grocerystorebackend.enums.VoucherType;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -17,6 +21,9 @@ public class Voucher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
 
     @Column(name = "code", nullable = false, columnDefinition = "varchar(50)")
     private String code;
@@ -32,4 +39,16 @@ public class Voucher {
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+    
+    @Enumerated(EnumType.STRING) // Lưu enum dưới dạng String trong DB
+    @Column(name = "type", columnDefinition = "varchar(50)")
+    private VoucherType type;
+
+    @OneToMany(mappedBy = "voucher", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<UserVoucher> userVouchers;
+
+    @OneToMany(mappedBy = "voucher", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Order> orders;
 }
